f8d681ade3e39ae629e0795d4b1992f0d5fdc8c5
KolokolPolovina <118216290+KolokolPolovina@users.noreply.github.com>
2023-03-28 22:18:47 +0500

    Update urls.py
    ис правил на router_v1

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -4,11 +4,11 @@ from rest_framework.routers import DefaultRouter
 from .views import UserViewSet, get_jwt_token, register
 
 
-router = DefaultRouter()
-router.register(r"users", UserViewSet)
+router_v1 = DefaultRouter()
+router_1.register(r"users", UserViewSet)
 
 urlpatterns = [
-    path('v1/', include(router.urls)),
+    path('v1/', include(router_1.urls)),
     path('v1/auth/signup/', register, name='register'),
     path('v1/auth/token/', get_jwt_token, name='token')
 ]
