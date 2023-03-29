d4def1b1ec926212e00ba435ecb99fb4182742f5
Дмитрий Рябков <demitry@e1.ru>
2023-03-29 12:33:13 +0500

    Readme added.

---
+++ api_yamdb/reviews/admin.py
@@ -0,0 +1,21 @@
+from django.contrib import admin
+
+from reviews.models import Comment, Review
+
+
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    """Админка для отзывов."""
+    list_display = ('id', 'text', 'author', 'pub_date', 'title', 'score')
+    search_fields = ('text', 'author')
+    list_filter = ('pub_date', 'author', 'title')
+    empty_value_display = '-пусто-'
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    """Админка для комментариев."""
+    list_display = ('id', 'text', 'author', 'pub_date', 'review')
+    search_fields = ('text', 'author')
+    list_filter = ('pub_date', 'author', 'review')
+    empty_value_display = '-пусто-'
