929d36b6ea3cbde67cc74d90b5055c8e1133f5d5
Александр Половинкин <kolokol-polovina@yandex.ru>
2023-03-30 12:38:27 +0500

    Добавил два пермишена.

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -8,3 +8,31 @@ class IsAdmin(permissions.BasePermission):
     def has_permission(self, request, view):
         return request.user.is_authenticated and (
             request.user.is_admin or request.user.is_superuser)
+
+
+class IsAdminOrReadOnly(permissions.BasePermission):
+    """
+    При небезопасном методе доступ разрешён только администратору.
+    """
+    def has_permission(self, request, view):
+        return (request.method in permissions.SAFE_METHODS
+                or (request.user.is_authenticated and (
+                    request.user.is_admin or request.user.is_superuser)))
+
+
+class IsAuthorModeratorAdminOrReadOnly(permissions.BasePermission):
+    """
+    Доступ разрешён всем при безопасном иетоде,
+    аутентифицированные пользователи могут использовать метод POST,
+    остальные методы могут исрользовать автор,
+    а также модератор, администратор и суперюзер.
+    """
+    def has_object_permission(self, request, view, obj):
+        return (
+            request.method in permissions.SAFE_METHODS
+            or request.method == 'POST' and request.user.is_authenticated
+            or obj.author == request.user
+            or request.user.is_admin
+            or request.user.is_moderator
+            or request.user.is_superuser
+        )
