fa74e438b6e167a963e43b9e92993547c0d3bbad
Dmitry Medvedev <mds@mail.ru>
2023-03-30 09:07:55 +0500

    fix some model attributes from PR#5

--- api_yamdb/reviews/models/base_models.py
+++ api_yamdb/reviews/models/base_models.py
@@ -5,6 +5,7 @@ User = get_user_model()
 
 
 class Category(models.Model):
+    """Категории (типы) произведений."""
     name = models.CharField(max_length=100, verbose_name='Название категории')
     slug = models.SlugField(max_length=40, unique=True)
 
@@ -13,6 +14,7 @@ class Category(models.Model):
 
 
 class Genre(models.Model):
+    """Категории жанров."""
     name = models.CharField(max_length=100, verbose_name='Название жанра')
     slug = models.SlugField(max_length=40, unique=True)
 
@@ -21,6 +23,9 @@ class Genre(models.Model):
 
 
 class Title(models.Model):
+    """
+    Произведения, к которым пишут отзывы (определённый фильм, книга и т.д.)
+    """
     name = models.CharField(max_length=200)
     year = models.IntegerField(verbose_name='Год выпуска',)
     description = models.TextField(max_length=3000)
@@ -28,17 +33,18 @@ class Title(models.Model):
         Category,
         on_delete=models.SET_NULL,
         related_name='categories',
+        blank=True,
         null=True,
         verbose_name='Категория',
         help_text='Категория, к которой относится произведение',
     )
     genre = models.ManyToManyField(
         Genre,
+        blank=True,
         related_name='genres',
         verbose_name='Жанр',
         help_text='Жанр произведения',
     )
-    
-        def __str__(self):
+
+    def __str__(self):
         return self.name
-        
