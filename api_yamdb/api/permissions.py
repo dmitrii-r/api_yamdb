65d441dd0e36f78bed820732408535a3bf14f771
Александр Половинкин <kolokol-polovina@yandex.ru>
2023-03-31 14:06:23 +0500

    Во время разрешения конфликта появился лишний пробел, пришлось убрать.

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -30,7 +30,7 @@ class IsAuthorModeratorAdminOrReadOnly(permissions.BasePermission):
     def has_permission(self, request, view):
         return (request.method in permissions.SAFE_METHODS
                 or request.user.is_authenticated)
-                
+
     def has_object_permission(self, request, view, obj):
         return (
             request.method in permissions.SAFE_METHODS
