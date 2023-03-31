abbd19dff36fe1b9bd212db8abc12a758e0f9995
Дмитрий Рябков <demitry@e1.ru>
2023-03-31 23:39:23 +0500

    Refactor Review.

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -8,3 +8,34 @@ class IsAdmin(permissions.BasePermission):
     def has_permission(self, request, view):
         return request.user.is_authenticated and (
             request.user.is_admin or request.user.is_superuser)
+
+
+class IsAdminOrReadOnly(permissions.BasePermission):
+    """
+    При небезопасном методе доступ разрешён только администратору.
+    """
+    def has_permission(self, request, view):
+        return (request.method in permissions.SAFE_METHODS
+                or (request.user.is_authenticated and (
+                    request.user.is_admin or request.user.is_superuser)))
+
+
+class IsAuthorModeratorAdminOrReadOnly(permissions.BasePermission):
+    """
+    Доступ разрешён всем при безопасном методе,
+    аутентифицированные пользователи могут использовать метод POST,
+    остальные методы могут использовать автор,
+    а также модератор, администратор и суперюзер.
+    """
+    def has_permission(self, request, view):
+        return (request.method in permissions.SAFE_METHODS
+                or request.user.is_authenticated)
+
+    def has_object_permission(self, request, view, obj):
+        return (
+            request.method in permissions.SAFE_METHODS
+            or obj.author == request.user
+            or request.user.is_admin
+            or request.user.is_moderator
+            or request.user.is_superuser
+        )
