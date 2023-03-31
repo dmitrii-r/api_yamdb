cb7f9b0c5e418e096bb8ac5944fb33d7438f7a36
Александр Половинкин <kolokol-polovina@yandex.ru>
2023-03-31 13:01:03 +0500

    Исправил все последние  замечания.

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -22,15 +22,18 @@ class IsAdminOrReadOnly(permissions.BasePermission):
 
 class IsAuthorModeratorAdminOrReadOnly(permissions.BasePermission):
     """
-    Доступ разрешён всем при безопасном иетоде,
+    Доступ разрешён всем при безопасном методе,
     аутентифицированные пользователи могут использовать метод POST,
-    остальные методы могут исрользовать автор,
+    остальные методы могут использовать автор,
     а также модератор, администратор и суперюзер.
     """
+    def has_permission(self, request, view):
+        return (request.method in permissions.SAFE_METHODS
+                or request.user.is_authenticated)
+
     def has_object_permission(self, request, view, obj):
         return (
             request.method in permissions.SAFE_METHODS
-            or request.method == 'POST' and request.user.is_authenticated
             or obj.author == request.user
             or request.user.is_admin
             or request.user.is_moderator
