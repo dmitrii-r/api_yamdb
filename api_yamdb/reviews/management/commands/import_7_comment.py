984a4a3ccf330da748f84b5d80cae5005f64d24c
PWSteal <39002058+PWSteal@users.noreply.github.com>
2023-04-01 11:20:41 +0500

    Заполнение базы данными из csv (#14)
    * add 4 tables import
    
    * add title-genre many-to-many table
    
    * add reviews comments db import
    
    * add import all command
    
    * add minor query optimization and add option to keep existing users
    
    * add encoding argument for open() to mitigate encoding issues
    
    * fixed formatting
    
    ---------
    
    Co-authored-by: Dmitry Medvedev <mds@mail.ru>

---
+++ api_yamdb/reviews/management/commands/import_7_comment.py
@@ -0,0 +1,41 @@
+import csv
+from django.core.management.base import BaseCommand
+from django.conf import settings
+from django.contrib.auth import get_user_model
+
+
+from reviews.models import Comment, Review
+
+User = get_user_model()
+
+
+class Command(BaseCommand):
+    """Заполняет базу данных образцами комментариев.
+    1. Очищает таблицу комментариев в базе данных от всех строк.
+    2. Импортирует данные из указанного csv-файла, подставляет их в поля
+    модели и заполняет базу новыми объектами.
+    Запуск команды: python3 manage.py import_7_comment
+    """
+
+    def handle(self, *args, **kwargs):
+        Comment.objects.all().delete()
+        csv_file_path = settings.BASE_DIR / 'static/data/comments.csv'
+        comments = []
+        with open(csv_file_path, 'r', encoding='utf-8') as file:
+            reader = csv.DictReader(file)
+            for row in reader:
+                new_object = Comment()
+                new_object.id = row['id']
+                new_object.text = row['text']
+                new_object.pub_date = row['pub_date']
+
+                review_id = int(row['review_id'])
+                review = Review.objects.get(id=review_id)
+                new_object.review = review
+
+                author_id = int(row['author'])
+                author = User.objects.get(id=author_id)
+                new_object.author = author
+
+                comments.append(new_object)
+        Comment.objects.bulk_create(comments)
