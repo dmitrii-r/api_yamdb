e962a4b0bf2b0bde7704d4a8a3eadbcb822dc4a0
Dmitry Medvedev <mds@mail.ru>
2023-04-02 11:30:33 +0500

    add Genre viewset

--- api_yamdb/api/serializers/category_genre_serializers.py
+++ api_yamdb/api/serializers/category_genre_serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from reviews.models import Category
+from reviews.models import Category, Genre
 
 
 class CategorySerializer(serializers.ModelSerializer):
@@ -8,4 +8,12 @@ class CategorySerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Category
+        fields = ('name', 'slug')
+
+
+class GenreSerializer(serializers.ModelSerializer):
+    """Сериализатор для жанров произведений."""
+
+    class Meta:
+        model = Genre
         fields = ('name', 'slug')
--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,8 +1,14 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-from api.views import (CategoryViewSet, CommentViewSet, ReviewViewSet,
-                       UserViewSet, get_jwt_token, register)
+from api.views import (CategoryViewSet,
+                       GenreViewSet,
+                       CommentViewSet,
+                       ReviewViewSet,
+                       UserViewSet,
+                       get_jwt_token,
+                       register
+                       )
 
 
 app_name = 'api'
@@ -10,6 +16,7 @@ app_name = 'api'
 router_v1 = DefaultRouter()
 router_v1.register(r"users", UserViewSet)
 router_v1.register(r"categories", CategoryViewSet)
+router_v1.register(r"genres", GenreViewSet)
 router_v1.register(
     r'titles/(?P<title_id>\d+)/reviews', ReviewViewSet, basename='reviews'
 )
