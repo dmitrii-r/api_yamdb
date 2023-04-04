fa375e9a1d8f294591cc7dee13c1ae7400a8643e
Дмитрий Рябков <118190172+dmitrii-r@users.noreply.github.com>
2023-04-04 18:34:29 +0500

    Small fixes. (#25)

--- api_yamdb/api/views/__init__.py
+++ api_yamdb/api/views/__init__.py
@@ -1,4 +1,4 @@
-from .user_views import *
-from .review_comment_views import *
-from .category_genre_views import *
-from .title_views import *
+from .user_views import *               # noqa: F401, F403
+from .review_comment_views import *     # noqa: F401, F403
+from .category_genre_views import *     # noqa: F401, F403
+from .title_views import *              # noqa: F401, F403
--- api_yamdb/reviews/models/__init__.py
+++ api_yamdb/reviews/models/__init__.py
@@ -1,2 +1,2 @@
-from .base_models import Category, Genre, Title
-from .review_comment_models import Comment, Review
+from .base_models import Category, Genre, Title     # noqa: F401, F403
+from .review_comment_models import Comment, Review  # noqa: F401, F403
